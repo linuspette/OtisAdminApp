c11a383ab41c07b8a61f64a703ed7828c770099a
linuspette <70110168+linuspette@users.noreply.github.com>
2022-11-29 14:01:53 +0100

    Added some logging to make troubleshooting easier

--- OtisAdminApp/Program.cs
+++ OtisAdminApp/Program.cs
@@ -8,6 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Logging.SetMinimumLevel(LogLevel.Warning);
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IApiService, ApiService>();
 builder.Services.AddScoped<ILinkService, LinkService>();
