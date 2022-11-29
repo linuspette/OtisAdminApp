b4457e241d9f5ae9e66d3a17dced6d7410ca1f0b
linuspette <70110168+linuspette@users.noreply.github.com>
2022-11-29 14:24:46 +0100

    Changed the HttpClient layout

--- OtisAdminApp/Services/ApiService.cs
+++ OtisAdminApp/Services/ApiService.cs
@@ -11,35 +11,37 @@ public interface IApiService
 public class ApiService : IApiService
 {
     private readonly ILogger<ApiService> _logger;
-    private readonly HttpClient _httpClient;
     private readonly string _baseAdress = "https://otisagileapi.azurewebsites.net/api/";
 
     public ApiService(ILogger<ApiService> logger)
     {
         _logger = logger;
-        _httpClient = new HttpClient();
     }
 
     //Get Method
     public async Task<string> GetAsync(string apiCall, IDictionary<string, string>? header)
     {
-        try
+        using (var _httpClient = new HttpClient())
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, _baseAdress + apiCall);
-            if (header != null)
+            try
             {
-                request.Headers.Add(header.Keys.First(), header.Values.First());
+                var request = new HttpRequestMessage(HttpMethod.Get, _baseAdress + apiCall);
+                if (header != null)
+                {
+                    request.Headers.Add(header.Keys.First(), header.Values.First());
+                }
+
+
+                var result = await _httpClient.SendAsync(request) ?? null!;
+
+                return await result.Content.ReadAsStringAsync();
             }
-
-
-            var result = await _httpClient.SendAsync(request) ?? null!;
-
-            return await result.Content.ReadAsStringAsync();
+            catch (Exception ex)
+            {
+                var message = ex.Message;
+            }
         }
-        catch (Exception ex)
-        {
-            var message = ex.Message;
-        }
+
 
         return null!;
     }
@@ -47,36 +49,43 @@ public class ApiService : IApiService
     //Post Method
     public async Task<string> PostAsync(string apiRoute, dynamic data)
     {
-        try
+        using (var _httpClient = new HttpClient())
         {
-            var request = new StringContent(data, Encoding.UTF8, "application/json");
+            try
+            {
+                var request = new StringContent(data, Encoding.UTF8, "application/json");
 
 
-            var result = await _httpClient.PostAsync(_baseAdress + apiRoute, request);
-            _logger.LogWarning($"Result from api: {await result.Content.ReadAsStringAsync()}");
-            return await result.Content.ReadAsStringAsync();
+                var result = await _httpClient.PostAsync(_baseAdress + apiRoute, request);
+                _logger.LogWarning($"Result from api: {await result.Content.ReadAsStringAsync()}");
+                return await result.Content.ReadAsStringAsync();
+            }
+            catch { }
+            return null!;
+
         }
-        catch { }
-        return null!;
     }
 
     //Delete Method
     public async Task<string> DeleteAsync(string apiCall, IDictionary<string, string> header)
     {
-        try
+        using (var _httpClient = new HttpClient())
         {
-            var request = new HttpRequestMessage(HttpMethod.Delete, _baseAdress + apiCall);
-
-            foreach (var variable in header)
+            try
             {
-                request.Headers.Add(variable.Key, header.Values);
+                var request = new HttpRequestMessage(HttpMethod.Delete, _baseAdress + apiCall);
+
+                foreach (var variable in header)
+                {
+                    request.Headers.Add(variable.Key, header.Values);
+                }
+
+                var result = await _httpClient.SendAsync(request);
+
+                return await result.Content.ReadAsStringAsync();
             }
-
-            var result = await _httpClient.SendAsync(request);
-
-            return await result.Content.ReadAsStringAsync();
+            catch { }
+            return null!;
         }
-        catch { }
-        return null!;
     }
 }
